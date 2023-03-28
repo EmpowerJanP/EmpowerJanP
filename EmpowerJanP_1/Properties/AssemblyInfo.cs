4e8c903420f34fdb5a82a5ddb36d538e1d2f9aa6
jpala <jpala@DESKTOP-71D0G48>
2023-03-28 14:24:49 +0200

    Add project files.

---
+++ EmpowerJanP_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerJanP_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerJanP_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("4D229A24-AC8B-4E68-B18D-D6D1CF46FFC6")]
+[assembly: AssemblyVersion("1.0.0.0")]
